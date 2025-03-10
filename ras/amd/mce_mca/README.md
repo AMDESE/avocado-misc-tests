35e86d33632b385fdc0ec327c3bd0ac664cc8dfe
Kalpana Shetty <kalpana.shetty@amd.com>
2025-03-10 14:35:21 +0530

    Add ras/mce_mca test cases and README.md
    Add ras/mce_mca test cases and README.md
    
    Signed-off-by: Smita Koralahalli Channabasappa <Smita.KoralahalliChannabasappa@amd.com>
    Signed-off-by: Kalpana Shetty <kalpana.shetty@amd.com>
    Reviewed-by: Avadhut Naik <avadhut.naik@amd.com>

---
+++ ras/amd/mce_mca/README.md
@@ -0,0 +1,55 @@
+# Pre-requisites
+        - Install msr-tools
+                $ apt-get install msr-tools    ## Use respective distro package install command
+
+# Note(s)
+1. Tests like check_deferred_handler.py, look for "LVT" in the boot time dmesg log. If the boot time dmesg is cleared or has truncted log may
+   cause failure in this test case, hence now a check has been added to look for "LVT" in 'journalctl' log in addition to dmesg log.
+
+2. ras tests should be run with supervisor permission(ex: the root user).
+
+# Test case information
+These are MCE/MCA test cases. Please run using "avocado run <test_name>"
+
+1. Module Name - check_deferred_handler
+   Description: Check if deferred error interrupt handler is installed
+   How to run:
+        # cd BM/ras/mce_mca
+        # avocado run check_deferred_handler.py
+
+2. Module Name: check_mca_config
+   Description: This module tests if MCA_CONFIG[McaXEnable] = 1 and
+       MCA_CONFIG[DeferredIntType] = 01b whereas other fields
+       are at "Reset" or "Init: BIOS" values, across all banks
+       for each logical CPU.
+   How to run:
+        # cd BM/ras/mce_mca
+        # avocado run check_mca_config.py
+
+3. Module Name = check_threshold_handler.py
+   Description: Check if thresholding interrupt handler is installed
+   How to run:
+        # cd BM/ras/mce_mca
+        # avocado run check_threshold_handler.py
+
+4. Module Name = mce_sysfs_init
+   Description: Check for mce sysfs device initialization
+   How to run:
+        # cd BM/ras/mce_mca
+        # avocado run mce_sysfs_init.py
+
+# Known Issues
+  msr-tools issue with more than 255 CPUs
+  ras/mce_mca tests will fail if it runs on a system with more than 255 CPUs. The issue is with 'msr-tools' installed from linux distribution.
+  The issue is fixed in upstream 'msr-tools'. It is recommended to install upstream 'msr-tools' tool to get ras/mce_mca tests runs successfully.
+
+        Upstream msr-tools: https://github.com/intel/msr-tools.git
+
+        Steps to build and install upstream 'msr-tools' on Ubuntu 24.04:
+               # Pre-requisites packages to be installed
+                     # apt-get -qq --yes install libtool
+                # git clone https://github.com/intel/msr-tools.git
+                # cd msr-tools
+                # ./autogen.sh
+                # make install
+#EOF
---
+++ ras/amd/mce_mca/systeminfo.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2025 Advanced Micro Devices, Inc.
+# @Author:   Smita Koralahalli Channabasappa <Smita.KoralahalliChannabasappa@amd.com>
+# @Module Name: systeminfo.py
+# @Description: Covers common helper functions.
+# @History:  Created Jan 10 2025 - Created
+
+import os
+import logging
+import subprocess
+from subprocess import call, Popen, PIPE
+from avocado.utils import linux_modules, distro
+from avocado.utils.software_manager.manager import SoftwareManager
+
+def Run(cmd):
+    """
+    Run a cmd[], return the exit code, stdout, and stderr.
+    """
+    proc=subprocess.Popen(cmd, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE, shell=True, universal_newlines=True)
+    out, err = proc.communicate()
+    return proc.returncode, out, err
+
+def install_preq():
+    """
+    Install msr-tools from source
+    """
+    logging.info("Installing pre-requisites packages")
+    sm = SoftwareManager()
+    dist = distro.detect()
+    deps = ['libtool']
+    if dist.name in ['Ubuntu', 'rhel']:
+        for package in deps:
+            if not sm.check_installed(package) and not sm.install(package):
+                logging.error("%s is needed for the qos tests to run" % package)
+    else:
+        logging.error("Unsupported distro")
+        return -1
+
+    # Install msr-tools from source
+    logging.info("Installing msr-tools package")
+    exitcode, stdout, _ = Run("git clone https://github.com/intel/msr-tools.git")
+    if (exitcode == 0):
+        exitcode, stdout, _ = Run("cd msr-tools; ./autogen.sh; make install")
+        if (exitcode == 0):
+            exitcode, stdout, _ = Run("rm -rf msr-tools")
+            logging.info(stdout)
+
+def check_module_load(mod, mod_config):
+    """
+    Check for module config and load if not loaded
+    """
+    ret = 0
+    config_check = linux_modules.check_kernel_config(mod_config)
+    if config_check == linux_modules.ModuleConfig.NOT_SET:
+        ret = -1
+        logging.info('Config %s is not set', mod_config)
+    else:
+        if linux_modules.load_module('msr'):
+            logging.info('Module Load Success')
+        else:
+            ret = 1
+            logging.info('Module Load Failed')
+
+    return ret
+
+def get_current_equivalent_id():
+    """
+    Returns the current equivalent_id in reg eax at function 0x80000001 in
+    cpuid
+    """
+    cpuid_info = Popen(["cpuid", "-1", "-r"], stdout=PIPE,
+            universal_newlines=True)
+    cpuid = Popen(["grep", "0x80000001"], stdin=cpuid_info.stdout,
+            stdout=PIPE, universal_newlines=True).communicate()[0]
+    cpuid_info.stdout.close()
+    cpu_id = cpuid.split(" ")
+    eq_id = cpu_id[5][4:14]
+    return eq_id
+
+def get_cpu_info():
+    """
+    Returns Number of thread(s) per core, number of core(s) per socket
+    and number of socket(s)
+    """
+    lscpu_info = Popen(["lscpu"], stdout=PIPE, universal_newlines=True)
+    cpu_info = Popen(["grep", "-E", "'|^Thread|^Core|^Socket'"],
+            stdin=lscpu_info.stdout, stdout=PIPE,
+            universal_newlines=True).communicate()[0]
+    lscpu_info.stdout.close()
+    cpu_info = cpu_info.strip('\n').split('\n')
+    return cpu_info
+
+if __name__ ==  "__main__":
+    main()
