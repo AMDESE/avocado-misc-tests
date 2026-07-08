0fe19833ee1218b9d067478f953c13d3e49a2f14
suryasaimadhu <SaiMadhu.KoyyalaHariVenkata@amd.com>
2026-07-08 12:34:33 +0530

    qos/pqos: Add opencloudos support for dependency installation
    opencloudos is an RPM-based distribution similar to RHEL. Include
    'opencloudos' in the supported distribution list so that required
    dependencies are installed on opencloudos systems for qos/pqos tests.
    
    Signed-off-by: suryasaimadhu <SaiMadhu.KoyyalaHariVenkata@amd.com>

--- qos/pqos/lib/qos_common.py
+++ qos/pqos/lib/qos_common.py
@@ -34,7 +34,7 @@ class CommonLib:
         sm = SoftwareManager()
         dist = distro.detect()
         deps = ['libtool']
-        if dist.name in ['Ubuntu', 'rhel', 'debian', 'anolis', 'openEuler']:
+        if dist.name in ['Ubuntu', 'rhel', 'debian', 'anolis', 'openEuler', 'opencloudos']:
             for package in deps:
                 if not sm.check_installed(package) and not sm.install(package):
                     logging.error("%s is needed for the qos tests to run" % package)
